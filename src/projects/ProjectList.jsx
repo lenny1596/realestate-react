af673f77b2f80f7245890c13f121450358fc2b09
lenny1596 <leninbose1596@gmail.com>
2023-06-06 21:46:28 +0530

    passing prop value got from form comp. to parent comp. as a prop

--- src/projects/ProjectList.jsx
+++ src/projects/ProjectList.jsx
@@ -1,30 +1,46 @@
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Project } from "./Project";
 import ProjectCard from "./ProjectCard";
 import ProjectForm from "./ProjectForm";
 
-// This is a component that takes in an object with a key of "projects" and a value of an array of projects.
-// For each project(object) in the "projects" array, a div is created with a key of the project's "id" property and a class of "cols-sm".
-// Within this div is a ProjectCard component that receives the current project as a prop.
-// A ProjectForm component is also created.
-const ProjectList = ({ projects }) => {
+/**
+ * This component takes a 'projects' array of 'Project' instances as props and returns a list of cards with the project name, description, and budget.
+ * Created state editProject to store the project object that is being edited.
+ * HandleEdit function is called when the edit button is clicked.
+ * if project.id matches editProject.id, display the form component otherwise display the card.
+ **/
+const ProjectList = ({ projects, onSave }) => {
+  const [editProject, setEditProject] = useState({});
+
+  const HandleEdit = (projectedited) => {
+    setEditProject(projectedited);
+  };
+
+  const HandleCancel = () => {
+    setEditProject({});
+  };
+
   return (
     <div className="row">
       {projects.map((project) => (
         <div key={project.id} className="cols-sm">
-          <ProjectCard project={project} />
-          <ProjectForm />
+          {project.id === editProject.id ? (
+            <ProjectForm onCancel={HandleCancel} onSave={onSave} />
+          ) : (
+            <ProjectCard project={project} onEdit={HandleEdit} />
+          )}
         </div>
       ))}
     </div>
   );
 };
 
-// Define the PropTypes for the ProjectList component
-// Validate that the `projects` prop is an array of `Project` instances
-// and require it to be present.
+/** Define the PropTypes for the ProjectList component &
+validates that the `projects` prop is an array of `Project` instances and require it to be present.**/
 ProjectList.propTypes = {
   projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired,
+  onSave: PropTypes.func.isRequired,
 };
 
 export default ProjectList;
