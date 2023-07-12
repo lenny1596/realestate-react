3fbf26da82002dfbfeebc1c44f0775b050e9ef38
lenny1596 <leninbose1596@gmail.com>
2023-07-12 19:23:52 +0530

    Adding form validation to form comp. and display err msg in JSX

--- src/projects/ProjectForm.jsx
+++ src/projects/ProjectForm.jsx
@@ -4,6 +4,11 @@ import { Project } from "./Project";
 
 const ProjectForm = ({ project: initialProject, onCancel, onSave }) => {
   const [project, setProject] = useState(initialProject);
+  const [errors, setErrors] = useState({
+    name: "",
+    description: "",
+    budget: "",
+  });
 
   function handleChange(event) {
     const { type, name, value, checked } = event.target;
@@ -22,10 +27,32 @@ const ProjectForm = ({ project: initialProject, onCancel, onSave }) => {
       updatedProject = new Project({ ...prevProject, ...change });
       return updatedProject;
     });
+    setErrors(() => validate(updatedProject));
+  }
+
+  function validate(project) {
+    /** you shouldn't change the state variable's object properties directly which is why a new errors variable is created and default values are set.*/
+    let errors = { name: "", description: "", budget: "" };
+    if (project.name.length === 0) errors.name = "Name is required";
+    if (project.name.length > 0 && project.name.length < 3)
+      errors.name = "Name must be at least 3 characters";
+    if (project.description.length === 0)
+      errors.description = "Description is required";
+    if (project.budget === 0) errors.budget = "Budget must be more than $0";
+    return errors;
+  }
+
+  function isValid() {
+    return (
+      errors.name.length === 0 &&
+      errors.description.length === 0 &&
+      errors.budget.length === 0
+    );
   }
 
   function handleFormSubmit(event) {
     event.preventDefault();
+    if (!isValid()) return;
     onSave(project);
   }
 
@@ -39,6 +66,11 @@ const ProjectForm = ({ project: initialProject, onCancel, onSave }) => {
         value={project.name}
         onChange={handleChange}
       />
+      {errors.name.length > 0 && (
+        <div className="card error">
+          <p>{errors.name}</p>
+        </div>
+      )}
       <label htmlFor="description">Project Description</label>
       <textarea
         name="description"
@@ -46,6 +78,11 @@ const ProjectForm = ({ project: initialProject, onCancel, onSave }) => {
         value={project.description}
         onChange={handleChange}
       />
+      {errors.description.length > 0 && (
+        <div className="card error">
+          <p>{errors.description}</p>
+        </div>
+      )}
       <label htmlFor="budget">Project Budget</label>
       <input
         type="number"
@@ -54,6 +91,11 @@ const ProjectForm = ({ project: initialProject, onCancel, onSave }) => {
         value={project.budget}
         onChange={handleChange}
       />
+      {errors.budget.length > 0 && (
+        <div className="card error">
+          <p>{errors.budget}</p>
+        </div>
+      )}
       <label htmlFor="isActive">Active?</label>
       <input
         type="checkbox"
@@ -72,7 +114,7 @@ const ProjectForm = ({ project: initialProject, onCancel, onSave }) => {
   );
 };
 
-/** This specifies that the 'onSave' & 'onCancel' props must be functions. 
+/** This specifies that the 'onSave' & 'onCancel' props must be functions.
  * The project prop must be an instance of Project. **/
 ProjectForm.propTypes = {
   project: PropTypes.instanceOf(Project),
