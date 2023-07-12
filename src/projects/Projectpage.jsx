eafcb8c21849eea833631a4b2f31a13a9c20b98d
lenny1596 <leninbose1596@gmail.com>
2023-07-12 22:30:08 +0530

    adding data to state from api & added css for loading animation

--- src/projects/Projectpage.jsx
+++ src/projects/Projectpage.jsx
@@ -1,13 +1,31 @@
-import React, { useState, Fragment } from "react";
-import { Mock_Projects } from "./MockProjects";
+import React, { useState, Fragment, useEffect } from "react";
 import ProjectList from "./ProjectList";
+import { projectAPI } from "./projectAPI";
 
 /** This component is a page for displaying a list of projects.
-* This function returns a component that displays the page's title and a ProjectList component that takes in Mock_Projects as props. 
-* function saveProject is called when the save button is clicked and the project is updated in the Mock_Projects array.
-**/
+ * This function returns a component that displays the page's title and a ProjectList component that takes in Mock_Projects as props.
+ * function saveProject is called when the save button is clicked and the project is updated in the Mock_Projects array.
+ **/
 const ProjectPage = () => {
-  const [projects, setProjects] = useState(Mock_Projects);
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(undefined);
+
+  useEffect(() => {
+    async function loadProjects() {
+      setLoading(true);
+      try {
+        const data = await projectAPI.get(1);
+        setError(null);
+        setProjects(data);
+      } catch (e) {
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadProjects();
+  }, []);
 
   function saveProject(project) {
     let updatedProjects = projects.map((p) => {
@@ -19,7 +37,25 @@ const ProjectPage = () => {
   return (
     <Fragment>
       <h1>Projects</h1>
+      {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse"></span>
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
       <ProjectList projects={projects} onSave={saveProject} />
+      {loading && (
+        <div className="center-page">
+          <span className="spinner primary"></span>
+          <p>Loading...</p>
+        </div>
+      )}
     </Fragment>
   );
 };
