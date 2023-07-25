3768b6bc9b46b3fe9826a4189352d097d6bcb2a2
lenny1596 <leninbose1596@gmail.com>
2023-07-25 14:52:46 +0530

    Adding Home route to App comp.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import "./App.css";
 import ProjectPage from "./projects/ProjectPage";
+import HomePage from "./home/HomePage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
-      <ProjectPage />
-    </div>
+    <Router>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
